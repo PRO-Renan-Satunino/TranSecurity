b05599ce508acb15562bbc94bede5120d8082d6a
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2025-06-26 00:49:03 -0300

    Update cadastro_usuario.py

--- views/cadastro_usuario.py
+++ views/cadastro_usuario.py
@@ -1,3 +1,10 @@
+"""
+Nome do arquivo: cadastro_usuario.py
+Equipe: Renan, Átila, Caio, Wagner, Adriano
+Turma: B91210
+Semestre: 2025.1
+"""
+
 import tkinter as tk
 from tkinter import messagebox
 from models.usuario import Usuario
