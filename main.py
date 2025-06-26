2f6418c80d5e828b0bb3a5c558c11145f3f5ccd9
Renan J. Saturnino <161033810+PRO-Renan-Satunino@users.noreply.github.com>
2025-06-26 00:51:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+Nome do arquivo: main.py
+Equipe: Renan, Átila, Caio, Wagner, Adriano
+Turma: B91210
+Semestre: 2025.1
+"""
+
 from views.menu_principal_view import MenuPrincipalView
 
 if __name__ == "__main__":
